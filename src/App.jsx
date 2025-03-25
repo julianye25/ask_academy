91dcfbde2c51fc3ab37f90777c6c1671ca18fdf6
Julian Yepes <julianyepes2593@gmail.com>
2025-03-24 22:57:27 -0500

    Arreglo en el nombre de un archivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Quiz from './components/quiz';
+import QuizApp from './components/quizApp';
 
 function App() {
   return (
     <div className=" font-mono">
-      <Quiz />
+      <QuizApp />
     </div>
   );
 }
